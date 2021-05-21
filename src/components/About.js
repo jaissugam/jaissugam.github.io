0bdb68c3749afecad15655b5a7fec3e5bc5e4900
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2021-05-22 01:40:47 +0545

    Updated styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './about.css';
 const About=()=>{
     return (
         <Card className="about">
-            <Card.Header as="h5">Namaste!</Card.Header>
+            <Card.Header as="h5"><span></span></Card.Header>
             <Card.Body>
                 <Card.Title>I'm a Computer Science Student from Nepal currently studying in VIT, India.I'm in this field of work because I like exploring the tech world and I enjoy the tech world since I'm in this field.I'm highly interested in software development side of things and specifically my enthusiasm lies towards the field of Web Development and Artificial Intelligence.Currently, I'm bettering myself to become a competent Web Developer.  </Card.Title>
                 
