4d9688facb0d6475ee34a193d0226bb08cbea527
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2021-05-22 18:35:23 +0545

    Font updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About=()=>{
         <Card className="about">
             <Card.Header as="h5"><span></span></Card.Header>
             <Card.Body>
-                <Card.Title>I'm a Computer Science Student from Nepal currently studying in VIT, India.I'm in this field of work because I like exploring the tech world and I enjoy the tech world since I'm in this field.I'm highly interested in software development side of things and specifically my enthusiasm lies towards the field of Web Development and Artificial Intelligence.Currently, I'm bettering myself to become a competent Web Developer.  </Card.Title>
+                <Card.Title style={{fontFamily: 'Caveat',fontSize:'30px'}}>I'm a Computer Science Student from Nepal currently studying in VIT, India.I'm in this field of work because I like exploring the tech world and I enjoy the tech world since I'm in this field.I'm highly interested in software development side of things and specifically my enthusiasm lies towards the field of Web Development and Artificial Intelligence.Currently, I'm bettering myself to become a competent Web Developer.  </Card.Title>
                 
                 <Button variant="primary" style={{borderRadius:'5px',color:'rgb(219, 199, 163)',backgroundColor:'black'}}>This</Button>
             </Card.Body>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,7 +6,7 @@ const Slider=()=>{
         <div className="content">
         <div className="slider-wrapper">
           <h3>Sugam Jaiswal</h3>
-          <div className="slider">
+          <div className="slider text-wrapper">
               <div className="slider-text1">LEARNER</div>
               <div className="slider-text2">PROGRAMMER</div>
               <div className="slider-text3">WEB DEVELOPER</div>
