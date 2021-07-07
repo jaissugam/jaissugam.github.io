cba012bbf348132dea3fd81ac9dd4df3aa4680d5
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2021-07-07 18:20:25 +0545

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,12 @@ const Footer=()=>{
             <Card.Header as="h5" style={{textAlign:'center'}}>If you wanna connect</Card.Header>
             <Card.Body style={mycard}>
             <ListGroup horizontal>
-                <ListGroup.Item ><a style={{color:'blue'}} rel="noreferrer" target='_blank' href='https://www.facebook.com/sugam.jaiswal'><i className="fab fa-facebook fa-2x"></i></a></ListGroup.Item>
-                <ListGroup.Item ><a style={{color:'#bc2a8d'}} rel="noreferrer" target='_blank' href='https://www.instagram.com/sgm4u/'><i className="fab fa-instagram fa-2x"></i></a></ListGroup.Item>
-                <ListGroup.Item ><a style={{color:'#00acee'}} rel="noreferrer" target='_blank' href='https://twitter.com/itsmesugam'><i className="fab fa-twitter-square fa-2x"></i></a></ListGroup.Item>
-                <ListGroup.Item ><a style={{color:'black'}} rel="noreferrer" target='_blank' href='https://github.com/jaissugam'><i className="fab fa-github-square fa-2x"></i></a></ListGroup.Item>
-                <ListGroup.Item ><a style={{color:'#0e76a8'}} rel="noreferrer" target="_blank" href='https://www.linkedin.com/in/sugam-jaiswal-31a27a1a3/'><i className="fab fa-linkedin fa-2x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'blue'}} rel="noreferrer" target='_blank' href='https://www.facebook.com/sugam.jaiswal'><i className="fab fa-facebook fa-1.5x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'#bc2a8d'}} rel="noreferrer" target='_blank' href='https://www.instagram.com/sgm4u/'><i className="fab fa-instagram fa-1.5x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'#00acee'}} rel="noreferrer" target='_blank' href='https://twitter.com/itsmesugam'><i className="fab fa-twitter fa-1.5x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'black'}} rel="noreferrer" target='_blank' href='https://github.com/jaissugam'><i className="fab fa-github fa-1.5x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'#0e76a8'}} rel="noreferrer" target="_blank" href='https://www.linkedin.com/in/sugam-jaiswal-31a27a1a3/'><i className="fab fa-linkedin fa-1.5x"></i></a></ListGroup.Item>
+                <ListGroup.Item ><a style={{color:'#0e76a8'}} rel="noreferrer" target="_blank" href='mailto:sgmjaiswal@gmail.com'><i className="fas fa-envelope fa-1.5x"></i></a></ListGroup.Item>
             </ListGroup>
             <Card.Subtitle style={{marginTop:'10px'}}>Crafted by sgm with 💓</Card.Subtitle>
             </Card.Body>
