af9e874a8e23ec78b8c2179d4ec5e0676b88d4c1
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2024-12-16 20:32:16 -0800

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About=()=>{
         <Card className="about">
             <Card.Header as="h5" style={{textAlign:'center'}}><span></span></Card.Header>
             <Card.Body>
-                <Card.Title style={{fontFamily: 'Caveat',fontSize:'30px'}}>I am a computer science scholar with a Bachelor’s degree in CSE from VIT'22, India and currently pursuing a Master’s degree in CSSE at UW'24. I am fascinated by the technological realm and its endless possibilities. I enjoy developing software solutions for various domains, especially web development and artificial intelligence. You could say I am a web aficionado and an AI enthusiast.  </Card.Title>
+                <Card.Title style={{fontFamily: 'Caveat',fontSize:'30px'}}>I am a software engineer with a master's in computer science from the University of Washington ('24) and a bachelor's degree in CSE from the VIT University ('22). I am fascinated by the technological realm and its endless possibilities. I enjoy developing and researching software solutions for various domains, especially web development and artificial intelligence. In simpler words, I am a web aficionado and an AI enthusiast. In my spare time, I like travelling, reading and (at times) cooking! </Card.Title>
                 
                 <Button variant="primary" style={{borderRadius:'5px',color:'rgb(219, 199, 163)',backgroundColor:'black',fontSize:'1.5rem'}}><a target="_blank" rel="noreferrer" href={resume}>View my Resume</a></Button>
             </Card.Body>
