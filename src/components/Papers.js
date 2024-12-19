cc3ae1cf4b71a044df2126040fa490f770c2c13b
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2024-12-19 00:47:00 -0800

    Update project descriptions

--- src/components/Papers.js
+++ src/components/Papers.js
@@ -5,23 +5,36 @@ import './Project.css';
 const Papers = () => {
     return (
         <Card className="papers">
-            <Card.Header as="h5" style={{textAlign:'center'}}>Research Papers</Card.Header>
+            <Card.Header as="h5" style={{textAlign:'center'}}>Recent Publications</Card.Header>
             <Card.Body style={{backgroundColor:"#343a40"}}>
                 <Project 
                     title='Paper Title 1' 
                     gitlink='https://github.com/jaissugam/Paper1' 
                     weblink='https://example.com/paper1'
-                    info='Description of Paper 1'></Project>
+                    info='Description of Paper 1'>
+                    
+                </Project>
                 <Project 
                     title='Paper Title 2' 
                     gitlink='https://github.com/jaissugam/Paper2' 
                     weblink='https://example.com/paper2'
-                    info='Description of Paper 2'></Project>
+                    info='Description of Paper 2'>
+                    
+                </Project>
                 <Project 
                     title='Paper Title 3' 
                     gitlink='https://github.com/jaissugam/Paper3' 
                     weblink='https://example.com/paper3'
-                    info='Description of Paper 3'></Project>
+                    info='Description of Paper 3'>
+                
+                </Project>
+                <Project 
+                    title='Paper Title 3' 
+                    gitlink='https://github.com/jaissugam/Paper3' 
+                    weblink='https://example.com/paper3'
+                    info='Description of Paper 3'>
+                
+                </Project>
             </Card.Body>
         </Card>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,27 +11,27 @@ const Projects=()=>{
                 title='Book Recommendation System' 
                 gitlink='https://github.com/jaissugam/Book-Recommendation-System' 
                 weblink='https://sugamjais.pythonanywhere.com/'
-                info='The webapp and the underlying recommender model works to recommend content-wise most similar books upon input using Cosine Similarity.'></Project>
+                info='The webapp and the underlying recommender model works to recommend content-wise most similar books based on input using Cosine Similarity.'></Project>
                 <Project 
                 title='Scalable Image Recognition using Vocabulary Tree' 
                 gitlink='https://github.com/jaissugam/Scalable-Recognition-using-Vocabulary-Tree' 
-                info='The project implements heirarchical k-means clustering and feature detector algorithms to create enhanced Vocabulary Trees for Real-Time Object Recognition in Image and Video Streams'></Project>
+                info='The project implements heirarchical k-means clustering and CV feature detector algorithms to create enhanced Vocabulary Trees for Real-Time Object Recognition in Image and Video Streams'></Project>
                 <Project 
                 title='Offensive Texts Classification' 
                 gitlink='https://github.com/jaissugam/Offensive-Text-Identification' 
                 weblink='http://offlang.pythonanywhere.com/'
-                info='It implements classification models like Naive Bayes and LSTM to classify offensive tweets.'></Project>
+                info='It implements classification models like Naive Bayes and LSTM to classify offensive tweets, and provides an interactive UI for the same.'></Project>
                 <Project 
                 pic={spacex}
                 title='SpaceX WebApp' 
                 gitlink='https://github.com/jaissugam/React-spaceX-App' 
                 weblink='https://spacex1.netlify.app/'
-                info='The app is built to provide a user interface to access the list of programs conducted by SpaceX since 2015.The data is taken from the publicly available spaceX API and the total of 4 apis have been used to implement vaious filters.'></Project>
+                info='The app is built to provide a user interface to access the list of programs conducted by SpaceX since 2015.The data is taken from the publicly available spaceX API and the total of 4 APIs were used to implement vaious filters.'></Project>
                 <Project 
                 title='ToDo List' 
                 gitlink='https://github.com/jaissugam/todolist' 
                 weblink='https://jaissugam.github.io/todolist/'
-                info='Simple todo web app with basic vue implementation Vue is a front-end JS framework which provides flexibility in UI designing.'></Project>
+                info='Simple TODO Web application with basic Vue JS implementation.'></Project>
                 
             </Card.Body>
         </Card>
